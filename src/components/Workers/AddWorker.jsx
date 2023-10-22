3edb9e0c09802c12501dfe8c6a258ab351cf0b13
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2023-10-23 00:15:19 +0300

    Add-Button-Card

--- src/components/Workers/AddWorker.jsx
+++ src/components/Workers/AddWorker.jsx
@@ -1,28 +1,33 @@
+import Button from "../UL/Button";
+import Card from "../UL/Card";
+
 function AddWorker() {
   return (
-    <form className="flex flex-col gap-y-2">
-      <label htmlFor="name" className="font-medium">
-        Çalışan İsmi
-      </label>
-      <input
-        type="text"
-        className="max-w-[40rem] w-full mx-auto border p-2"
-        placeholder="Çalışan İsmi Yazınız."
-        id="name"
-      />
-      <label htmlFor="wage" className="font-medium">
-        Maaş Miktarı
-      </label>
-      <input
-        type="text"
-        className="max-w-[40rem] w-full mx-auto border p-2"
-        placeholder="Maaş  Miktarı Giriniz."
-        id="wage"
-      />
-      <button className="p-2 text-lg mt-2 bg-teal-700 text-white" type="submit">
-        Ekle
-      </button>
-    </form>
+    <Card className={"mt-10"}>
+      <form className="flex flex-col gap-y-2">
+        <label htmlFor="name" className="font-medium">
+          Çalışan İsmi
+        </label>
+        <input
+          type="text"
+          className="max-w-[40rem] w-full mx-auto border p-2"
+          placeholder="Çalışan İsmi Yazınız."
+          id="name"
+        />
+        <label htmlFor="wage" className="font-medium">
+          Maaş Miktarı
+        </label>
+        <input
+          type="number"
+          className="max-w-[40rem] w-full mx-auto border p-2"
+          placeholder="Maaş  Miktarı Giriniz."
+          id="wage"
+        />
+        <Button className="mt-2" type="submit">
+          Ekle
+        </Button>
+      </form>
+    </Card>
   );
 }
 
